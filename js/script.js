fd0a2d30e6fd7fbba74f70964fa9ea6dc11d722f
Owen <owenac555@gmail.com>
2025-10-27 09:03:30 +0700

    Done

--- js/script.js
+++ js/script.js
@@ -1,29 +1,77 @@
-function doNumber() {
+let totalNumber = Number.MIN_VALUE;
+let operator = "";
 
+function doOperation (getTextButton, numberDisplay) {
+    if (getTextButton === "=") {
+        calc("=", numberDisplay);
+        displayIt(totalNumber);
+    }
+    else {
+        calc(getTextButton, numberDisplay);
+    }
 }
 
-function doOperator() {
-
-}
-
-function deletePerChar() {
+function calc(getTextButton, numberDisplay) {
+    if (operator === "+") {
+        totalNumber += numberDisplay;
+    }
+    else if (operator === "-") {
+        totalNumber -= numberDisplay;
+    }
+    else if (operator === "*") {
+        totalNumber *= numberDisplay;
+    }
+    else if (operator === "/") {
+        totalNumber /= numberDisplay;
+    }
     
+    operator = getTextButton;
+}
+
+function displayIt(numberDisplay) {
+    const getScreen = document.getElementById("screen");
+    getScreen.value = numberDisplay;
+}
+
+function resetScreen() {
+    const getScreen = document.getElementById("screen");
+    getScreen.value = "";
 }
 
 (function() {
     const getCalculator = document.getElementById("calculator");
-    getCalculator.addEventListener("click", (event) => {
-        const getDataset = event.target.dataset.text;
-        const number = Number(getDataset);
+    let numberDisplay = "";
 
-        if (isNaN(number)) {
-            doNumber(number);
-        }
-        else if (getDataset === "del") {
-            deletePerChar();
-        }
-        else {
-            doOperator(number);
+    getCalculator.addEventListener("click", (event) => {
+        const getTextButton = event.target.dataset.text;
+
+        if (getTextButton !== undefined) {
+            if (isNaN(Number(getTextButton))) { //operator
+                if (getTextButton === "del") {
+                    operator = "";
+                    totalNumber = Number.MIN_VALUE;
+                    resetScreen();
+                }
+                else {
+                    if (operator === "") {
+                        operator = getTextButton;
+                        totalNumber = Number(numberDisplay);
+                    }
+                    else {
+                        doOperation(getTextButton, Number(numberDisplay));
+                    }
+                    numberDisplay = "";
+                }
+
+            }
+            else { //number
+                numberDisplay += getTextButton;
+                displayIt(numberDisplay);
+
+                if (operator === "=") {
+                    operator = "";
+                }
+            }
         }
     });
 }());
